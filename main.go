eac3131577df111ae5af7edde0e69dffacac45dc
Joel Pagliuca <pagliucajoel@gmail.com>
2018-04-09 19:55:38 +1000

    attempt 1 at reading from the pipe. I'm a failure

--- main.go
+++ main.go
@@ -2,8 +2,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
+	"os"
 	"strings"
 
 	"github.com/atotto/clipboard"
@@ -11,34 +14,43 @@ import (
 
 var version = "to be set by makefile"
 
+func convertText(toConvert string, doClipboard bool, doZalgo bool) {
+	output := toConvert
+	// do the conversion
+	if doZalgo {
+		output = zalgo(output)
+	} else {
+		output = vapor(output)
+	}
+	if doClipboard {
+		clipboard.WriteAll(output)
+	} else {
+		fmt.Printf("%s\n", output)
+	}
+}
+
 func main() {
 	// get input flags
 	sendToClip := flag.Bool("c", false, "Copy output to clipboard")
 	zalgoInput := flag.Bool("z", false, "Make the text Zalgo")
 	flag.Parse()
 	// read in the string from input
-	inputText := flag.Args()
-
-	// combine into single string
-	var buffer strings.Builder
-	var space string
-	for _, v := range inputText {
-		buffer.WriteString(space)
-		buffer.WriteString(v)
-		space = " "
-	}
-	output := buffer.String()
-
-	// do the conversion
-	if *zalgoInput {
-		output = zalgo(output)
+	stdinInfo, _ := os.Stdin.Stat()
+	var inputReader *bufio.Reader
+	if stdinInfo.Size() > 0 {
+		fmt.Println("stdin")
+		// read from pipe
+		inputReader = bufio.NewReader(os.Stdin)
+		for {
+			inputText, err := inputReader.ReadString('\n')
+			if err != nil && err == io.EOF {
+				break
+			}
+			convertText(inputText, *sendToClip, *zalgoInput)
+		}
 	} else {
-		output = vapor(output)
-	}
-	if *sendToClip {
-		clipboard.WriteAll(output)
-	} else {
-		fmt.Printf("%s\n", output)
+		inputText := strings.Join(flag.Args(), " ")
+		convertText(inputText, *sendToClip, *zalgoInput)
 	}
 
 }
