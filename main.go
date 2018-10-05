dc1b4df429c1fdb81eecdb9c065fc931c1d9dec4
Wilhelm Eek <wilhelm@spaceship.com.au>
2018-10-05 10:37:28 +1000

    Make cli

--- main.go
+++ main.go
@@ -2,41 +2,44 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"strings"
 
 	"github.com/atotto/clipboard"
+	survey "gopkg.in/AlecAivazis/survey.v1"
 )
 
 var version = "to be set by makefile"
 
 func main() {
-	// get input flags
-	sendToClip := flag.Bool("c", false, "Copy output to clipboard")
-	zalgoInput := flag.Bool("z", false, "Make the text Zalgo")
-	flag.Parse()
-	// read in the string from input
-	inputText := flag.Args()
+	input := struct {
+		SendToClip bool   `survey:"clip"`
+		ZalgoInput bool   `survey:"zalgo"`
+		Text       string `survey:"text"`
+	}{}
 
-	// combine into single string
-	var buffer strings.Builder
-	var space string
-	for _, v := range inputText {
-		buffer.WriteString(space)
-		buffer.WriteString(v)
-		space = " "
-	}
-	output := buffer.String()
+	survey.Ask([]*survey.Question{
+		{
+			Name:   "clip",
+			Prompt: &survey.Confirm{Message: "Send to clip?"},
+		},
+		{
+			Name:   "zalgo",
+			Prompt: &survey.Confirm{Message: "Zalgo?"},
+		},
+		{
+			Name:   "text",
+			Prompt: &survey.Input{Message: "Input:"},
+		},
+	}, &input)
 
-	// do the conversion
-	if *zalgoInput {
+	output := input.Text
+	if input.ZalgoInput {
 		output = zalgo(output)
 	} else {
 		output = vapor(output)
 	}
 
-	if *sendToClip {
+	if input.SendToClip {
 		clipboard.WriteAll(output)
 	} else {
 		fmt.Printf("%s\n", output)
