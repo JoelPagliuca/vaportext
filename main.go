d309cc21ebf90d7561840be8151d7dd57da7d8b2
Wilhelm Eek <wilhelm@spaceship.com.au>
2018-10-05 19:11:56 +1000

    Make interactive mode a thing

--- main.go
+++ main.go
@@ -2,7 +2,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"strings"
 
 	"github.com/atotto/clipboard"
 	survey "gopkg.in/AlecAivazis/survey.v1"
@@ -11,26 +13,46 @@ import (
 var version = "to be set by makefile"
 
 func main() {
-	var input struct {
+	interactive := flag.Bool("i", false, "interactive")
+	sendToClip := flag.Bool("c", false, "Copy output to clipboard")
+	zalgoInput := flag.Bool("z", false, "Make the text Zalgo")
+	flag.Parse()
+
+	inputText := flag.Args()
+	var buffer strings.Builder
+	var space string
+	for _, v := range inputText {
+		buffer.WriteString(space)
+		buffer.WriteString(v)
+		space = " "
+	}
+
+	input := struct {
 		SendToClip bool   `survey:"clip"`
 		ZalgoInput bool   `survey:"zalgo"`
 		Text       string `survey:"text"`
+	}{
+		SendToClip: *sendToClip,
+		ZalgoInput: *zalgoInput,
+		Text:       buffer.String(),
 	}
 
-	survey.Ask([]*survey.Question{
-		{
-			Name:   "clip",
-			Prompt: &survey.Confirm{Message: "Send to clip?"},
-		},
-		{
-			Name:   "zalgo",
-			Prompt: &survey.Confirm{Message: "Zalgo?"},
-		},
-		{
-			Name:   "text",
-			Prompt: &survey.Input{Message: "Input:"},
-		},
-	}, &input)
+	if *interactive {
+		survey.Ask([]*survey.Question{
+			{
+				Name:   "clip",
+				Prompt: &survey.Confirm{Message: "Send to clip?"},
+			},
+			{
+				Name:   "zalgo",
+				Prompt: &survey.Confirm{Message: "Zalgo?"},
+			},
+			{
+				Name:   "text",
+				Prompt: &survey.Input{Message: "Input:"},
+			},
+		}, &input)
+	}
 
 	output := input.Text
 	if input.ZalgoInput {
